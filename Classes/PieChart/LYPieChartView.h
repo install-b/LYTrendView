1e6bb0b11e15be68407d26e666a39d7f4a3aa397
Shangen Zhang <gkzhangshangen@163.com>
2018-06-06 21:34:41 +0800

    remove flies

---
+++ Classes/PieChart/LYPieChartView.h
@@ -0,0 +1,13 @@
+//
+//  LYPieChartView.h
+//  LYTrendView
+//
+//  Created by Shangen Zhang on 2018/6/6.
+//  Copyright © 2018年 Shangen Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LYPieChartView : UIView
+
+@end
---
+++ Classes/PieChart/LYPieChartView.m
@@ -0,0 +1,21 @@
+//
+//  LYPieChartView.m
+//  LYTrendView
+//
+//  Created by Shangen Zhang on 2018/6/6.
+//  Copyright © 2018年 Shangen Zhang. All rights reserved.
+//
+
+#import "LYPieChartView.h"
+
+@implementation LYPieChartView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
